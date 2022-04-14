3c85649bc9accbcf669eaed279bbd3a030790b10
毛 <1296193245@qq.com>
2022-04-14 18:30:53 +0800

    axios_ajax_request

---
+++ src/main/java/mao/axios_ajax_request/AxiosAjaxRequestApplication.java
@@ -0,0 +1,15 @@
+package mao.axios_ajax_request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxiosAjaxRequestApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AxiosAjaxRequestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/axios_ajax_request/AxiosAjaxRequestApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.axios_ajax_request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxiosAjaxRequestApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
